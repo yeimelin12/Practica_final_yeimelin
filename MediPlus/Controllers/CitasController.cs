787ed210ec3a485c34a661271f99238fc250c8f8
jcayetano <jcayetano@apap.com.do>
2021-08-15 11:16:28 -0400

    initial commit

---
+++ MediPlus/Controllers/CitasController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MediPlus.Models;
+
+namespace MediPlus.Controllers
+{
+    public class CitasController : Controller
+    {
+        private readonly CITASMEDICASDBContext _context;
+
+        public CitasController(CITASMEDICASDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Citas
+        public async Task<IActionResult> Index()
+        {
+            var cITASMEDICASDBContext = _context.Citas.Include(c => c.IdMedicoNavigation).Include(c => c.IdPacientesNavigation);
+            return View(await cITASMEDICASDBContext.ToListAsync());
+        }
+
+        // GET: Citas/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cita = await _context.Citas
+                .Include(c => c.IdMedicoNavigation)
+                .Include(c => c.IdPacientesNavigation)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (cita == null)
+            {
+                return NotFound();
+            }
+
+            return View(cita);
+        }
+
+        // GET: Citas/Create
+        public IActionResult Create()
+        {
+            ViewData["IdMedico"] = new SelectList(_context.Medicos, "Id", "Oficina");
+            ViewData["IdPacientes"] = new SelectList(_context.Pacientes, "Id", "Id");
+            return View();
+        }
+
+        // POST: Citas/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,IdPacientes,IdMedico,MotivoDeConsulta,FechaCita,Comentario,Estado")] Cita cita)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(cita);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdMedico"] = new SelectList(_context.Medicos, "Id", "Oficina", cita.IdMedico);
+            ViewData["IdPacientes"] = new SelectList(_context.Pacientes, "Id", "Id", cita.IdPacientes);
+            return View(cita);
+        }
+
+        // GET: Citas/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cita = await _context.Citas.FindAsync(id);
+            if (cita == null)
+            {
+                return NotFound();
+            }
+            ViewData["IdMedico"] = new SelectList(_context.Medicos, "Id", "Oficina", cita.IdMedico);
+            ViewData["IdPacientes"] = new SelectList(_context.Pacientes, "Id", "Id", cita.IdPacientes);
+            return View(cita);
+        }
+
+        // POST: Citas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,IdPacientes,IdMedico,MotivoDeConsulta,FechaCita,Comentario,Estado")] Cita cita)
+        {
+            if (id != cita.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(cita);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CitaExists(cita.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdMedico"] = new SelectList(_context.Medicos, "Id", "Oficina", cita.IdMedico);
+            ViewData["IdPacientes"] = new SelectList(_context.Pacientes, "Id", "Id", cita.IdPacientes);
+            return View(cita);
+        }
+
+        // GET: Citas/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cita = await _context.Citas
+                .Include(c => c.IdMedicoNavigation)
+                .Include(c => c.IdPacientesNavigation)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (cita == null)
+            {
+                return NotFound();
+            }
+
+            return View(cita);
+        }
+
+        // POST: Citas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var cita = await _context.Citas.FindAsync(id);
+            _context.Citas.Remove(cita);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CitaExists(int id)
+        {
+            return _context.Citas.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ MediPlus/Models/Medico.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace MediPlus.Models
+{
+    public partial class Medico
+    {
+        public Medico()
+        {
+            Cita = new HashSet<Cita>();
+            Secretaria = new HashSet<Secretarium>();
+        }
+
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        public int? Especialidad { get; set; }
+        public string Oficina { get; set; }
+        public string Telefono { get; set; }
+        public int IdUsuario { get; set; }
+
+        public virtual Especialidade EspecialidadNavigation { get; set; }
+        public virtual Usuario IdUsuarioNavigation { get; set; }
+        public virtual ICollection<Cita> Cita { get; set; }
+        public virtual ICollection<Secretarium> Secretaria { get; set; }
+    }
+}
